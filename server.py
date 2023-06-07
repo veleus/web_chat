8936549814fa312997b42283dd1e90900c761ab2
veleus <63227936+veleus@users.noreply.github.com>
2023-06-07 21:45:32 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,23 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 messages = []
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        username = request.form['username']
+        message = request.form['message']
+        messages.append({'username': username, 'message': message})
     return render_template('index.html', messages=messages)
 
-@app.route('/send_message', methods=['POST'])
-def send_message():
-    username = request.form['username']
-    message = request.form['message']
-    if username and message:
-        messages.append({'username': username, 'message': message})
-    return redirect(url_for('index'))
-
-@app.route('/get_messages', methods=['GET'])
-def get_messages():
-    return jsonify(messages)
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
